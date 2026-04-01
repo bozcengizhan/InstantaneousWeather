a88776e13e6fcfc62ed855121bc278d45b02c925
Cengizhan Boz <bozcengizhan2@gmail.com>
2026-04-01 16:55:02 +0300

    Uygulama ilkel düzeyde geliştirilmeye başlandı.

--- app/src/main/java/com/example/instantaneousweather/WeatherPage.kt
+++ app/src/main/java/com/example/instantaneousweather/WeatherPage.kt
@@ -1,8 +1,34 @@
 package com.example.instantaneousweather
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WeatherPage(){
 
+    Scaffold(containerColor = Color.LightGray) {
+        Column() {
+            Card(modifier = Modifier.padding(16.dp).border(3.dp,Color.Red)) {
+                Text(text = "Hello", modifier = Modifier.padding(16.dp))
+            }
+        }
+    }
+
+}
+
+
+@Preview
+@Composable
+fun WeatherPagePreview(){
+    WeatherPage()
 }
