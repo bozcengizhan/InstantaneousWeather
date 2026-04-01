da73706c78ec7ab74dc91a4accb877ef2b61ebed
Cengizhan Boz <bozcengizhan2@gmail.com>
2026-04-01 16:38:38 +0300

    first commit.

--- app/src/main/java/com/example/instantaneousweather/MainActivity.kt
+++ app/src/main/java/com/example/instantaneousweather/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.instantaneousweather.ui.theme.InstantaneousWeatherTheme
 
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             InstantaneousWeatherTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InstantaneousWeatherTheme {
-        Greeting("Android")
     }
 }
